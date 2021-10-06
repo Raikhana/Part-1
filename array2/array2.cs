36512e49a6b841d0c48aa2d2cbae3d3f9a94b8c2
Raikhana <raikhanaurazbaeva@gmail.com>
2021-10-06 10:05:53 -0600

    Sorting and copying  arrays

---
+++ array2/array2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array2
+{
+    class array2
+    {
+        static void Main(string[] args)
+        {
+            //Initializing and storing value in arrays
+            
+            Console.Write("\nan array #1:\t");
+            int[] arr1 = new int[5] { 43, 25, 33, 14, 10 };
+            printarray(arr1);
+
+            Console.Write("\nan array #2:\n");
+            int[] arr2 = new int[6];
+            int i;
+            for (i = 0; i < 6; i++)
+            {
+                Console.Write("Enter number:\t");
+                arr2[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            printarray(arr2);
+
+            //Sorting arrays
+            Console.Write("\n\nSORTING an array #1:\t");
+            Array.Sort(arr1);
+            printarray(arr1);
+            Console.WriteLine("Length:\t{0}", arr1.GetLength(0));
+            Console.WriteLine("Value of 3 element:\t{0}", arr1.GetValue(2));
+            Console.WriteLine("Index position of number 10:\t{0}", Array.IndexOf(arr1, 10));
+
+            Console.Write("\n\nSORTING an array #2:\t");
+            Array.Sort(arr2);
+            printarray(arr2);
+            Console.WriteLine("Length:\t{0}", arr2.GetLength(0));
+            Console.WriteLine("Value of 3 element:\t{0}", arr2.GetValue(2));
+            Console.WriteLine("Index position of number 10:\t{0}", Array.IndexOf(arr2, 10));
+
+            //Copying
+            Array.Copy(arr1, arr2, 3);
+            Console.WriteLine("\n\nCOPYING arr1's 3 numbers to arr2");
+            printarray(arr1);
+            printarray(arr2);
+
+            //Removing
+            Console.WriteLine("\nREMOVING numbers from arrays:");
+            Array.Clear(arr1, 0, 2);
+            Array.Clear(arr2, 2, 4);
+            Console.Write("an array 1:\t");
+            printarray(arr1);
+            Console.Write("an array 1:\t");
+            printarray(arr2);
+            Console.ReadLine();
+
+
+        }
+        //printing arrays
+        static void printarray(int[] arr)
+        {
+            Console.WriteLine("\nElements of array is:\n");
+            foreach (int i in arr)
+            {
+                Console.Write("\t{0}", i);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ loop/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+
+            //int emails = 20;
+            //while (emails >= 0)
+            //{
+            //    Console.WriteLine($"emails are left: {emails}");
+            //    emails--;
+            //}
+            //Console.WriteLine("INBOX ZERO ACHIEVED!");
+            //Console.ReadKey();
+
+
+
+            //2
+            //string str = "";
+            //bool buttonClick = true;
+            //do
+            //{
+            //    Console.WriteLine("BLARRRRRRRRR [stop] [no] :");
+            //    str = Console.ReadLine();
+            //} while (!buttonClick || str != "stop");
+
+
+
+            //3
+            //string ans;
+            //do
+            //{
+            //    Console.Write("Are you done? [yes] [no] : ");
+            //    ans = Console.ReadLine();
+            //} while (ans != "yes");
+
+
+
+
+            //4
+            //for (int i = 1; i < 17; i++)
+            //{
+            //    CreateTemplate(i);
+            //}
+            //Console.ReadKey();
+
+            ////5
+            //string[] todo = { "respond to email", "make wireframe", "program feature", "fix bugs" };
+            //foreach (string task in todo)
+            //{
+            //    CreateTodoItem(task);
+            //}
+            //Console.ReadKey();
+
+
+
+            string[] websites = { "twitter", "facebook", "gmail" };
+            int counter = 0;
+
+            while (counter < websites.Length)
+            {
+                Console.WriteLine(websites[counter]);
+                counter++;
+            }
+            Console.ReadLine();
+
+            string[] melody = { "a", "b", "c", "c", "b" };
+
+            foreach (string note in melody)
+            {
+                //PlayMusic(note);
+                Console.WriteLine($"[] {note}");
+            }
+            Console.ReadLine();
+
+           
+
+
+        }
+        // for 4
+        static void CreateTemplate(int week)
+        {
+            Console.WriteLine($"Week {week}");
+            Console.WriteLine("Announcements: \n \n \n ");
+            Console.WriteLine("Report Backs: \n \n \n");
+            Console.WriteLine("Discussion Items: \n \n \n");
+        }
+        //for 5
+        static void CreateTodoItem(string item)
+        {
+            Console.WriteLine($"[] {item}");
+        }
+
+
+
+    }
+}
